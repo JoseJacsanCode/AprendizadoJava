837cdbf8f97ad043e21e37b8a9e6ad9228da17d6
Jose Jacsan <jjacsan@gmail.com>
2026-06-18 19:56:14 -0300

    Primeiro commit

---
+++ src/tiposPrimitivos/Main.java
@@ -0,0 +1,23 @@
+package tiposPrimitivos;
+
+public class Main {
+    public static void main(String[] args) {
+        // números inteiros = 100, 156, 48878, 1000, 598999797
+        // números decimais = 1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9
+
+        // byte => 8 bits => -128 a 127
+        byte minhaVar = 127;
+
+        // short => 16 bits => -32.768 a 32.767
+        short minhaVar2 = 32767;
+
+        // int => 32 bits => -2.147.483.648 a 2.147.483.647
+        // long => 64 bits => -9.223.372.036.854.775.808 a 9.223.372.036.854.775.807
+        // float => 32 bits => precisão simples
+        // double => 64 bits => precisão dupla
+        // String => Representar palavras e frases
+        // char => representar um único caractere
+        // boolean => true ou false
+
+    }
+}
---
+++ src/variaveis/Main.java
@@ -0,0 +1,13 @@
+package variaveis;
+public class Main {
+    public static void main(String[] args) {
+       int minhaIdade = 26;
+       minhaIdade = 21;
+
+       String meuNome = "José";
+       meuNome = "Jacsan";
+
+       // tipo nomeDaVariavel = valor;
+       var meuNome2 = "José";
+    }
+}
